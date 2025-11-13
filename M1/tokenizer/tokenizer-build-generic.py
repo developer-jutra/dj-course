a0deff2decacec203ea6af0f3ff9958209358b8d
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-11-13 21:02:07 +0100

    feat: make generic function for creating tokenizers

--- M1/tokenizer/tokenizer-build-generic.py
+++ M1/tokenizer/tokenizer-build-generic.py
@@ -11,14 +11,13 @@ tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
 tokenizer.pre_tokenizer = Whitespace()
 
 # 3. Set the Trainer
-def create_trainer(vocab_size: int = 32000, min_frequency: int = 2) -> BpeTrainer:
-    return BpeTrainer(
-        special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"],
-        vocab_size=vocab_size,
-        min_frequency=min_frequency
-    )
+trainer = BpeTrainer(
+    special_tokens=["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]"],
+    vocab_size=32000,
+    min_frequency=2
+)
 
-def build_tokenizer(corpus_name: str, glob_pattern: str = "*.txt", dry_run: bool = True, vocab_size: int = 32000, min_frequency: int = 2):
+def build_tokenizer(corpus_name: str, glob_pattern: str = "*.txt", dry_run: bool = True):
     if not corpus_name:
         raise ValueError(f"Corpus {corpus_name} not provided")
 
@@ -28,14 +27,14 @@ def build_tokenizer(corpus_name: str, glob_pattern: str = "*.txt", dry_run: bool
     TOKENIZER_OUTPUT_FILE = f"tokenizers/tokenizer-{corpus_name.lower().replace("_", "-")}.json"
 
     if corpus_name == "ALL":
-        TOKENIZER_OUTPUT_FILE = f"tokenizers/tokenizer-all-corpora-{str(vocab_size).replace("000","k").lower()}-{str(min_frequency).lower()}x.json"
+        TOKENIZER_OUTPUT_FILE = f"tokenizers/tokenizer-all-corpora.json"
 
     if corpus_name == "WOLNELEKTURY" and len(glob_pattern) > len("*.txt"):
         TOKENIZER_OUTPUT_FILE = f"tokenizers/tokenizer-{glob_pattern.replace('-*.txt', '')}.json"
 
     # 4. Train the Tokenizer
     if not dry_run:
-        tokenizer.train(files=FILES, trainer=create_trainer(vocab_size, min_frequency))
+        tokenizer.train(files=FILES, trainer=trainer)
 
     # 5. Save the vocabulary and tokenization rules
     if not dry_run:
@@ -45,11 +44,7 @@ def build_tokenizer(corpus_name: str, glob_pattern: str = "*.txt", dry_run: bool
 
 if __name__ == "__main__":
     print("\ntokenizer_build:")
-    build_tokenizer("WOLNELEKTURY", "pan-tadeusz-*.txt", dry_run=True)
-    build_tokenizer("WOLNELEKTURY", "*.txt", dry_run=True)
-    build_tokenizer("NKJP", "*.txt", dry_run=True)
+    build_tokenizer("WOLNELEKTURY", "pan-tadeusz-*.txt", dry_run=False)
+    build_tokenizer("WOLNELEKTURY", "*.txt", dry_run=False)
+    build_tokenizer("NKJP", "*.txt", dry_run=False)
     build_tokenizer("ALL", "*.txt", dry_run=False)
-    build_tokenizer("ALL", "*.txt", dry_run=False, vocab_size=16000)
-    build_tokenizer("ALL", "*.txt", dry_run=False, vocab_size=64000)
-    build_tokenizer("ALL", "*.txt", dry_run=False, vocab_size=32000, min_frequency=1)
-    build_tokenizer("ALL", "*.txt", dry_run=False, vocab_size=32000, min_frequency=3)
