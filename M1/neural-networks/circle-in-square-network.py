4b7425bfd9658a9733ad8eda92174afeb27fa812
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2025-12-26 17:38:40 +0100

    feat: M1 jupyter solution (#5)
    * WIP: solution
    
    * chore: add output dir to gitignore
    
    * feat: prepare data for Bielik
    
    * feat: make generic function for creating tokenizers
    
    * feat: create tokenizers using generic function
    
    * feat: download tokenizer from hugging face
    
    * feat: create tokenizers with different parameters, compare results
    
    * refactor: split file into training and inferring part, run training
    
    * feat: embedding solution part 1
    
    * feat: embedding solution part 2
    
    * feat: embedding solution part 3
    
    * feat: ex.9 jupyter solution
    
    * feat: ex. 10 neural networks solution
    
    * feat: ex. 11 sequence diagrams solution

--- M1/neural-networks/circle-in-square-network.py
+++ M1/neural-networks/circle-in-square-network.py
@@ -19,12 +19,14 @@ writer = SummaryWriter(LOG_DIR)
 class CircleInSquareNet(nn.Module):
     def __init__(self):
         super(CircleInSquareNet, self).__init__()
-        self.fc1 = nn.Linear(2, 2)
-        self.fc2 = nn.Linear(2, 1)
+        self.fc1 = nn.Linear(2, 8)
+        self.fc2 = nn.Linear(8, 4)
+        self.fc3 = nn.Linear(4, 1)
 
     def forward(self, x):
         x = nn.ReLU()(self.fc1(x))
-        x = self.fc2(x)
+        x = nn.ReLU()(self.fc2(x))
+        x = self.fc3(x)
         return x
 
 np.random.seed(42)
@@ -47,7 +49,7 @@ def generate_circle_data(num_samples, radius=0.5):
     return X, Y
 
 # Generowanie danych
-NUM_SAMPLES = 10 # 🔥🔥🔥 PRACUJESZ TUTAJ
+NUM_SAMPLES = 250 # 🔥🔥🔥 PRACUJESZ TUTAJ
 X_circle, Y_circle = generate_circle_data(NUM_SAMPLES)
 
 # Inicjalizacja:
@@ -57,7 +59,7 @@ model2 = CircleInSquareNet()
 # model z sekcji 1
 
 # Krok 1: Definicja Parametrów
-LEARNING_RATE = 0.00001 # 🔥🔥🔥 PRACUJESZ TUTAJ
+LEARNING_RATE = 0.03 # 🔥🔥🔥 PRACUJESZ TUTAJ
 EPOCHS = 500 # 🔥🔥🔥 PRACUJESZ TUTAJ
 STEP = 100
 
